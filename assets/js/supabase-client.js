7c9ff1ef43a8476789e22ac33f6471f3ceafbfe9
appIeonlineport <indiatryme@gmail.com>
2026-08-19 03:52:49 +0530

    Configure NX Voice Supabase publishable key

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 export const SUPABASE_URL = 'https://jipmvlleiwgjgyskdciw.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'PASTE_SUPABASE_PUBLISHABLE_KEY_HERE';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_vXYqp4ysDOW3Q4Dlih7NtQ_cpPO335H';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
